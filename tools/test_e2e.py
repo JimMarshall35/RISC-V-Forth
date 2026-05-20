cd105d6b97c672aa9f57d2295eda36a5b0efbab4
Jim Marshall <jim.marshall@codethink.co.uk>
2026-05-20 20:00:50 +0100

    a few more tests added

--- tools/test_e2e.py
+++ tools/test_e2e.py
@@ -12,10 +12,11 @@ import pexpect
 
 # renamed to stop pytest thinking this is a test class
 class NotPyTestCase:
-    def __init__(self, input_strs, expected_data_stack, cleanup):
+    def __init__(self, input_strs, expected_data_stack, cleanup, testName="anonymous"):
         self.input_strs = input_strs
         self.expected_data_stack_string = expected_data_stack
         self.cleanup = cleanup
+        self.testName = testName
 
     def run(self, proc):
         for input_str in self.input_strs:
@@ -23,8 +24,8 @@ class NotPyTestCase:
         try:
             proc.expect_exact(self.expected_data_stack_string, timeout=10)
         except pexpect.TIMEOUT:
-            print(f"Timeout waiting for expected output: {self.expected_data_stack_string}. Input strings: {'\n'.join(self.input_strs)}")
-            assert False, "Test failed due to timeout"
+            print(f"Timeout waiting for expected output: {self.expected_data_stack_string}. Input strings: {'\n'.join(self.input_strs)} Test name: {self.testName}")
+            assert False, f"Test '{self.testName}' failed due to timeout"
         except pexpect.EOF:
             print(f"EOF for expected output: {self.expected_data_stack_string}. Input strings: {'\n'.join(self.input_strs)}")
             assert False, "EOF"
@@ -43,16 +44,95 @@ tests = [
     # the forth code checks for a carriage return to process the input line.
     # Windows terminal outputs \r\n and this code was developed on Windows, 
     # but the CI runs on Linux, so we need to add \r
-    NotPyTestCase(["1 2 show\r"], "[ 1, 2 ]", "drop drop show\r"),
-    NotPyTestCase(["1 2 + show\r"], "[ 3 ]", "drop show\r"),
-    NotPyTestCase(["4 6 - show\r"], "[ -2 ]", "drop show\r"),
-    NotPyTestCase([": jim 1 2 3 4 5 ;\r", "jim show\r"], "[ 1, 2, 3, 4, 5 ]", "drop drop drop drop drop show\r"),
-    NotPyTestCase([": jim2 jim 6 7 8 ;\r", "jim2 show\r"], "[ 1, 2, 3, 4, 5, 6, 7, 8 ]", "drop drop drop drop drop drop drop drop show\r"),
-    NotPyTestCase([": jim if 1 2 3 else 4 5 6 then ;\r", "1 jim\r", "0 jim show\r"], "[ 1, 2, 3, 4, 5, 6 ]", "drop drop drop drop drop drop show\r"),
-    NotPyTestCase([": begintest 0 begin 1 + dup 10 = if r then 0 until ;\r", "begintest show\r"], "[ 10 ]", "drop show\r"),
-    NotPyTestCase([": testloop 0 do i loop ;\r", "4 testloop show\r"], "[ 0, 1, 2, 3 ]", "drop drop drop drop show\r"),
+
+    # push literals and show 
+    NotPyTestCase(["1 2 show\r"], "[ 1, 2 ]", "drop drop show\r", 
+                  "push literals and show"),
+
+    # test +
+    NotPyTestCase(["1 2 + show\r"], "[ 3 ]", "drop show\r", 
+                  "test +"),
+
+    # test -
+    NotPyTestCase(["4 6 - show\r"], "[ -2 ]", "drop show\r", 
+                  "test -"),
+
+    # test : and ;
+    NotPyTestCase([": jim 1 2 3 4 5 ;\r", "jim show\r"], "[ 1, 2, 3, 4, 5 ]", "drop drop drop drop drop show\r",
+                  "test : and ;"),
+
+    # test one word being compiled in another
+    NotPyTestCase([": jim2 jim 6 7 8 ;\r", "jim2 show\r"], "[ 1, 2, 3, 4, 5, 6, 7, 8 ]", "drop drop drop drop drop drop drop drop show\r",
+                  "test one word being compiled in another"),
+
+    # test if / else
+    NotPyTestCase([": jim if 1 2 3 else 4 5 6 then ;\r", "1 jim\r", "0 jim show\r"], "[ 1, 2, 3, 4, 5, 6 ]", "drop drop drop drop drop drop show\r", 
+                  "test if / else"),
+
+    # test begin / until
+    NotPyTestCase([": begintest 0 begin 1 + dup 10 = if r then 0 until ;\r", "begintest show\r"], "[ 10 ]", "drop show\r",
+                  "test begin / until"),
+
+    # test do / loop
+    NotPyTestCase([
+        ": testloop 0 do i loop ;\r", 
+        "4 testloop show\r"
+    ], 
+    "[ 0, 1, 2, 3 ]", 
+    "drop drop drop drop show\r",
+    "test do / loop"),
     NotPyTestCase(["3 testloop show\r"], "[ 0, 1, 2 ]", "drop drop drop show\r"),
-    NotPyTestCase([": k 0 do i 2 mod if -1 else i then loop ;\r", "5 k show\r"], "[ 0, -1, 2, -1, 4 ]", "drop drop drop drop drop show\r")
+
+    # test do / loop and if / else together
+    NotPyTestCase([
+        ": k 0 do i 2 mod if -1 else i then loop ;\r",
+        "5 k show\r"
+    ], 
+    "[ 0, -1, 2, -1, 4 ]", 
+    "drop drop drop drop drop show\r",
+    "test do / loop and if / else together"),
+
+    # test create
+    NotPyTestCase([
+        ": var create , ;\r",
+        "42 var v1\r",
+        "v1 @ show\r",
+    ], 
+    "[ 42 ]",  # expected stack picture
+    "drop show\r",
+    "test create"), # cleanup stack
+
+    # test does>
+    NotPyTestCase([
+        ": const create , does> @ ;\r",
+        "56 const c1\r",
+        "c1 show\r",
+    ], 
+    "[ 56 ]", # expected stack picture
+    "drop show\r",
+    "test does>"), # cleanup stack
+
+    # test print int
+    NotPyTestCase([
+        "8 . cr\r"
+    ],
+    "0x00000008",
+    "show\r",
+    "test print int"),
+
+    # test multiline function
+    NotPyTestCase([
+        ": foo\r",
+        "  3 0 do\r",
+        "    i .\r",
+        "  loop\r",
+        "  cr\r",
+        ";\r",
+        "foo\r",
+    ],
+    "0x000000000x000000010x00000002",
+    "show\r",
+    "test multiline function"),
 ]
 
 def test_run():
@@ -70,7 +150,7 @@ def test_run():
         try:
             proc.expect("dict end: 0x[0-9a-fA-F]{8}", timeout=10)
         except pexpect.TIMEOUT:
-            print("Timeout waiting for 'Risc V Forth' prompt.")
+            print("Timeout waiting for 'dict end:' prompt.")
             assert False, "QEMU did not output expected prompt"
         except pexpect.EOF:
             print("pexpect.EOF")
