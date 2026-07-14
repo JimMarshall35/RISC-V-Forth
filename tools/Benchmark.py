3531c71e8dca30a7808e511d505844e2603f2609
Jim Marshall <jim.marshall@codethink.co.uk>
2026-07-14 08:56:05 +0100

    properly exit minicom

--- tools/Benchmark.py
+++ tools/Benchmark.py
@@ -41,6 +41,7 @@ forth_code_hash = ""
 
 N = 1000000
 EXPECTED_VAL = "0x928b54e2"
+proc = None
 
 def get_git_commit_hash() -> str:
     result = subprocess.run(
@@ -64,6 +65,7 @@ def load_forth_fibonacci_src(proc):
         proc.expect("\n")
 
 def collect_forth(args):
+    global proc
     with open(args.out,'wb') as logF:
         proc = None
         try:
@@ -91,6 +93,7 @@ def collect_forth(args):
     pass
 
 def collect_asm(args):
+    global proc
     with open(args.out,'wb') as logF:
         proc = None
         try:
@@ -156,5 +159,16 @@ def main():
 
     elif args.mode == "forth":
         collect_forth(args)
+
+    # Send Ctrl-A then 'x' to bring up exit dialog
+    proc.send("\x01")   # Ctrl-A
+    proc.send("x")
+    # minicom asks "Leave without reset?" - confirm
+    proc.expect("Leave")
+    proc.sendline("")
+
+    # Wait for the process to actually terminate
+    proc.expect(pexpect.EOF)
+    proc.close()
 
 main()
